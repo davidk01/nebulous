6a6c0534091503711798173248dddf8c7488689a
david karapetyan <davidk@palantir.com>
2015-05-14 16:29:31 -0700

    fix test

--- test/stagestest.rb
+++ test/stagestest.rb
@@ -54,7 +54,7 @@ class TestStages < Minitest::Test
       Stages::Script.new(File.expand_path(File.dirname __FILE__) + '/harness/script.sh', ['arg1', 'arg2', 'arg3'], 3)]
     stage_collection = Stages::StageCollection.new(*instances)
     stage_collection.generate_files
-    generated_files = Dir["tmp/stages/*"]
+    generated_files = Dir["tmp/stages/**"]
     ['stage-1.sh', 'stage-2.sh', 'stage-3.sh'].each do |file|
       assert(generated_files.any? {|f| f.include?(file)}, "We should have generated #{file}.")
     end
