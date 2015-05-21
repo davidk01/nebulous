ebb339683796c415550fb8b0a2fab532aa0284ef
david karapetyan <dkarapetyan@scriptcrafty.com>
2015-05-20 21:48:24 -0700

    forgot to add the error

--- lib/errors.rb
+++ lib/errors.rb
@@ -2,6 +2,6 @@
  :ForkingProvisionerDefinitionError, :MissingConfigurationParameterError, :NilConfigurationValueError, :NilVMError, :OpenNebulaTemplateError,
  :PathNilError, :PoolInformationError, :StageNumberNilError, :UnexpectedSecureValueError, :UnknownActionError,
  :UnknownConfigurationTypeError, :UnknownProvisioningStageError, :VMIPError, :UnknownConfigurationKeyValueError,
- :FileLocationError, :StageFileExistsError, :SeveralTemplatesMatchesError, :TemplateNotFoundError].each do |error|
+ :FileLocationError, :StageFileExistsError, :SeveralTemplatesMatchesError, :TemplateNotFoundError, :EmptyActionArray].each do |error|
   Object.const_set(error.to_s, Class.new(StandardError))
 end
